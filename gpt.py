2c9024b0c14aeaea1e141091ba9d347953613ff0
Ethan Vinh <eth.vinh@gmail.com>
2026-01-07 16:25:25 -0800

    broke down token reading into chunks and batches

--- gpt.py
+++ gpt.py
@@ -54,8 +54,35 @@ print(yb)
 
 print('----')
 
-for b in range(batch_size): # batch dimension
+"""for b in range(batch_size): # batch dimension
     for t in range(block_size): # time dimension
         context = xb[b, :t+1]
         target = yb[b,t]
-        print(f"when input is {context.tolist()} the target: {target}")
+        print(f"when input is {context.tolist()} the target: {target}")"""
+       
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+torch.manual_seed(1337)
+
+class BigramLanguageModel(nn.Module):
+
+    def __init__(self, vocab_size):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets=None):
+
+        # idx and targets are both (B,T) tensor of integers
+        logits = self.token_embedding_table(idx) # (B,T,C)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
